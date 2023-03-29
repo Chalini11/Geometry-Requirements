a8bab1e6999a0d261bbfdccc6c0212f729505990
Chalini11 <chalini.pasupathy@m2pfintech.com>
2023-03-29 15:52:08 +0530

    Req1: Add. logic and testcases implemented for finding the area of rectangle

--- src/main/java/com/m2pfintech/RectangleShape.java
+++ src/main/java/com/m2pfintech/RectangleShape.java
@@ -1,4 +1,12 @@
 package com.m2pfintech;
 
 public class RectangleShape {
+    public int area(int length, int breadth)
+    {
+        if(length < 0 || breadth < 0)
+            throw new ArithmeticException();
+        return length*breadth;
+
+    }
 }
+
--- src/test/java/com/m2pfintech/RectangleShapeTest.java
+++ src/test/java/com/m2pfintech/RectangleShapeTest.java
@@ -1,9 +1,31 @@
 package com.m2pfintech;
 
-import java.awt.*;
+import org.junit.jupiter.api.Test;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RectangleShapeTest
 {
     RectangleShape rectangleObject = new RectangleShape();
+
+    @Test
+
+    void ToReturnTwentyWhenGivenInputsTwoAndTen()
+    {
+        int expectedValue = 20;
+
+        int actualValue = rectangleObject.area(2,10);
+
+        assertEquals(expectedValue,actualValue);
+    }
+    @Test
+    void toThrowExceptionWhenEitherOfValuesNegative()
+    {
+        assertThrows(ArithmeticException.class,() -> rectangleObject.area(-2,-2));
+    }
+
 
 }
